a3f348a06ae239538d5e6f22f0f55b07b43d16e5
thanhtutoo <5531305090@lamduan.mfu.ac.th>
2016-09-21 18:02:22 +0700

    User section working 100 percentage update properly

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -113,6 +113,13 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name','id')->all();
+
+
+        return view('admin.users.edit', compact('user','roles'));
+
     }
 
     /**
@@ -125,6 +132,51 @@ class AdminUsersController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        } else{
+
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+
+
+
+        if($file = $request->file('photo_id')){
+
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+
+
+        $user->update($input);
+
+
+        return redirect('/admin/users');
+
+
+
+
+
     }
 
     /**
